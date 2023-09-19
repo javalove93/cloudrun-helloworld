7410e9a17527bbf78aba43b1892d74fc32b54fc8
Jerry Jeong <javalove93@gmail.com>
2023-09-19 18:06:12 +0900

    qwiklabs

--- 00.prepare_container.sh
+++ 00.prepare_container.sh
@@ -1,4 +1,6 @@
-GOOGLE_CLOUD_PROJECT=custom-qwiklabs-jerry
+GOOGLE_CLOUD_PROJECT=qwiklabs-gcp-03-6f14832216cc
+gcloud auth configure-docker \
+    asia-east1-docker.pkg.dev
 docker pull javalove93/helloworld
-docker tag javalove93/helloworld gcr.io/${GOOGLE_CLOUD_PROJECT}/helloworld
-docker push gcr.io/${GOOGLE_CLOUD_PROJECT}/helloworld
+docker tag javalove93/helloworld asia-east1-docker.pkg.dev/${GOOGLE_CLOUD_PROJECT}/helloworld/helloworld
+docker push asia-east1-docker.pkg.dev/${GOOGLE_CLOUD_PROJECT}/helloworld/helloworld
--- 01.deploy_to_cloudrun.sh
+++ 01.deploy_to_cloudrun.sh
@@ -1,2 +1,3 @@
-GOOGLE_CLOUD_PROJECT=custom-qwiklabs-jerry
-gcloud run deploy --image=gcr.io/${GOOGLE_CLOUD_PROJECT}/helloworld --platform managed
+GOOGLE_CLOUD_PROJECT=qwiklabs-gcp-03-6f14832216cc
+gcloud run deploy --project ${GOOGLE_CLOUD_PROJECT} --region asia-east1 --image=asia-east1-docker.pkg.dev/${GOOGLE_CLOUD_PROJECT}/helloworld/helloworld --platform managed --allow-unauthenticated helloworld
+
